4093cc2ce3b82cf16a3affbb2560da8829118cbe
nitinverma <nitin.matrix@gmail.com>
2012-06-17 00:24:57 +0530

    decently working http get

--- node_http_client.js
+++ node_http_client.js
@@ -1,20 +1,72 @@
 var http = require('http');
 
-console.log('Welcome to socket pool.');
+console.log('Welcome to http client.');
+
+var request_options_array = [{
+    agent : false /*Making sure we have no connection pooling 'on'.*/, 
+    host: 'localhost', 
+    port : 8888,
+    path : '/',
+},
+{
+    agent : false /*Making sure we have no connection pooling 'on'.*/, 
+    host: 'localhost', 
+    port : 8888,
+    path : '/',
+},
+    {
+    agent : false /*Making sure we have no connection pooling 'on'.*/, 
+    host: 'localhost', 
+    port : 8888,
+    path : '/',
+},
+{
+    agent : false /*Making sure we have no connection pooling 'on'.*/, 
+    host: 'localhost', 
+    port : 8888,
+    path : '/',
+}
+];
 
 if (!nitin) {
     var nitin = {};
 }
 
 (function() {
-    if (!nitin['sockets']) {
-        nitin.sockets = {};
+    if (!nitin['http']) {
+        nitin.http = {};
     }
     
-    var agent = new http.Agent();
-    console.log(agent);
+    var onData = function(request, response) {
+        return function(data) {
+            console.log('RESPONSE: ' + data);
+        };
+    }
+
+    var onResponse = function(request) {
+        return function(response) {
+            response.on('data', onData(request, response));
+        };
+    }
+    
+    var onError = function(error) {
+      console.log('Error: ' + error);
+    }
+        
+    nitin.http.get = function(request_options_array) {
+        for (var i = 0; i < request_options_array.length; i += 1) {
+            request_options = request_options_array[i];
+            var request = http.get(request_options);
+            request.on('response', onResponse(request));
+            request.on('error', onError);
+        }
+    };
 
 })();
+
+
+
+nitin.http.get(request_options_array);
 
 console.log('===***===');
 
